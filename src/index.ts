51ca2a3595226ecbff76a0f2f1a802df87ca6995
LayNath242 <nath.selendra@gmail.com>
2022-09-01 11:48:05 +0700

    disable

--- src/index.ts
+++ src/index.ts
@@ -24,14 +24,14 @@ async function publicContract(contractAddress: string) {
   return hash.toHex()
 }
 
-Promise.resolve()
-  .then(async () => {
-    const result = await publicContract('0x3e88d2d5AaBC9DC54d5d84636De87874a6B5FfD5');
-    logger.info(result);
-  })
-  .then(async () => {
-    process.exit();
-  })
-  .catch(async (error) => {
-    logger.error(error);
-  });
+// Promise.resolve()
+//   .then(async () => {
+//     const result = await publicContract('0x3e88d2d5AaBC9DC54d5d84636De87874a6B5FfD5');
+//     logger.info(result);
+//   })
+//   .then(async () => {
+//     process.exit();
+//   })
+//   .catch(async (error) => {
+//     logger.error(error);
+//   });
